4e4fa84e3db3cb53215294cae846a7e4b30a8ef2
Iver Martinsen <iver.martinsen@outlook.com>
2021-07-24 14:22:29 +0200

    added comments

--- Python scripts/model_sequential.py
+++ Python scripts/model_sequential.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Example of using Sequential models. 
+Example of using tf.keras.Sequential to build model. 
 
 Created on Fri May 21 11:29:41 2021
 
@@ -8,47 +8,61 @@ Created on Fri May 21 11:29:41 2021
 """
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, ReLU
-from tensorflow.keras.layers import BatchNormalization, Dropout
-from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import BatchNormalization, Dropout, InputLayer
 
 from tensorflow.keras.layers.experimental.preprocessing import RandomFlip
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 
+from tensorflow.keras.regularizers import L2
+
 Model = tf.keras.Sequential([
+    
     InputLayer(input_shape=(128, 128, 1)),
-    Rescaling(1./127.5, offset=-1),
+    
+    # preprocessing
+    Rescaling(1./255.),
+    
+    # data augmentation
     RandomFlip("horizontal"),
-    Conv2D(8, 3, strides = (1, 1), padding='same', kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    
+    # conv block 1
+    Conv2D(8, 3, padding='same', kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     MaxPool2D(),
     
-    Conv2D(16, 3, padding='same', kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    # conv block 2
+    Conv2D(16, 3, padding='same', kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     MaxPool2D(),
         
-    Conv2D(32, 3, padding='same', kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    # conv block 3
+    Conv2D(32, 3, padding='same', kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     MaxPool2D(),
 
-    Conv2D(64, 3, padding='same', kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    # conv block 4
+    Conv2D(64, 3, padding='same', kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     MaxPool2D(),
     
     Flatten(),
     
-    Dense(32, kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    # dense layer 1
+    Dense(32, kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     Dropout(0.5),
 
-    Dense(16, kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3)),
+    # dense layer 2
+    Dense(16, kernel_regularizer=L2(l2=1e-3)),
     BatchNormalization(),
     ReLU(),
     Dropout(0.5),
-
-    Dense(1, activation='sigmoid', kernel_regularizer=tf.keras.regularizers.L2(l2=1e-3))
+    
+    # output layer
+    Dense(1, activation='sigmoid', kernel_regularizer=L2(l2=1e-3))
     ])
