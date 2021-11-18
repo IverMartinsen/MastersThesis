390f0e229db3f3282c4e31a33a92f2e5c08badc3
Iver Martinsen <iver.martinsen@outlook.com>
2021-11-18 13:03:55 +0100

    minor style changes

--- Python/scripts/EFD.py
+++ Python/scripts/EFD.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Example of obtaining elliptical fourier discriptors (EFD's) 
+Example of obtaining elliptical fourier descriptors (EFD's)
 of an otolith image.
 
 Created on Thu Jul 22 11:53:00 2021
@@ -11,7 +11,7 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
-from image_tools import contour_img, chain_code
+from modules.efd import contour_img, chain_code
 
 # import and normalize image
 path = (r'C:\Users\iverm\Google Drive\Masteroppgave' + 
@@ -35,7 +35,7 @@ ax2.axis('off')
 _, thresholded = cv.threshold(blurred, 0.5, 1, cv.THRESH_BINARY)
 
 # display effect of thresholding
-fig, (ax1, ax2) = plt.subplots(1, 2)
+_, (ax1, ax2) = plt.subplots(1, 2)
 ax1.imshow(blurred, 'gray')
 ax1.set_title('Blurred image')
 ax1.axis('off')
@@ -47,7 +47,7 @@ ax2.axis('off')
 points, boundary = contour_img(thresholded, 0)
 
 # display boundary image
-fig, (ax1, ax2) = plt.subplots(1, 2)
+_, (ax1, ax2) = plt.subplots(1, 2)
 ax1.imshow(thresholded, 'gray')
 ax1.set_title('Thresholded image')
 ax1.axis('off')
@@ -62,9 +62,9 @@ code = chain_code(points)
 # Eqs. given by Giardina & Kuhl (1982).
 delta_t = 1 + ((np.sqrt(2) - 1) / 2)*(1 - (-1)**code)
 
-t = np.cumsum(delta_t)                # accumulated time vector
-t_min = np.concatenate(([0], t[:-1])) # shifted time vector
-T = t[-1]                             # fundamental period
+t = np.cumsum(delta_t)                 # accumulated time vector
+t_min = np.concatenate(([0], t[:-1]))  # shifted time vector
+T = t[-1]                              # fundamental period
 
 # Changes in projections along the x and y axis with
 # eqs. given by Giardina & Kuhl (1982).
@@ -72,33 +72,28 @@ T = t[-1]                             # fundamental period
 delta_x = np.sign(6 - code) * np.sign(2 - code)
 delta_y = np.sign(4 - code) * np.sign(code)
 
+
 # Functions for computing the Fourier coefficents for x and y
 # with eqs. given by Giardina & Kuhl (1982).
-a = lambda n: T * np.sum(
-    delta_x * (
-        np.cos(2*n*np.pi*t/T) - np.cos(2*n*np.pi*t_min/T)) / delta_t) / (
-            2*n**2*np.pi**2)
+def a(n):
+    return T * np.sum(delta_x * (np.cos(2*n*np.pi*t/T) - np.cos(2*n*np.pi*t_min/T)) / delta_t) / (2*n**2*np.pi**2)
 
-b = lambda n: T * np.sum(
-    delta_x * (
-        np.sin(2*n*np.pi*t/T) - np.sin(2*n*np.pi*t_min/T)) / delta_t) / (
-            2*n**2*np.pi**2)
 
-c = lambda n: T * np.sum(
-    delta_y * (
-        np.cos(2*n*np.pi*t/T) - np.cos(2*n*np.pi*t_min/T)) / delta_t) / (
-            2*n**2*np.pi**2)
+def b(n):
+    return T * np.sum(delta_x * (np.sin(2*n*np.pi*t/T) - np.sin(2*n*np.pi*t_min/T)) / delta_t) / (2*n**2*np.pi**2)
 
-d = lambda n: T * np.sum(
-    delta_y * (
-        np.sin(2*n*np.pi*t/T) - np.sin(2*n*np.pi*t_min/T)) / delta_t) / (
-            2*n**2*np.pi**2)
+
+def c(n):
+    return T * np.sum(delta_y * (np.cos(2*n*np.pi*t/T) - np.cos(2*n*np.pi*t_min/T)) / delta_t) / (2*n**2*np.pi**2)
+
+
+def d(n):
+    return T * np.sum(delta_y * (np.sin(2*n*np.pi*t/T) - np.sin(2*n*np.pi*t_min/T)) / delta_t) / (2*n**2*np.pi**2)
+
 
 # Computing the DC components for x and y using eqs. from Giardina & Kuhl.            
-epsilon = np.cumsum(
-    np.concatenate(([0], delta_x[:-1]))) - delta_x *np.cumsum(t_min) / delta_t            
-delta = np.cumsum(
-    np.concatenate(([0], delta_y[:-1]))) - delta_y *np.cumsum(t_min) / delta_t            
+epsilon = np.cumsum(np.concatenate(([0], delta_x[:-1]))) - delta_x * np.cumsum(t_min) / delta_t
+delta = np.cumsum(np.concatenate(([0], delta_y[:-1]))) - delta_y * np.cumsum(t_min) / delta_t
 
 A0 = np.sum(delta_x*(t**2-t_min**2)/(2*delta_t)+epsilon*(t-t_min))/T
 C0 = np.sum(delta_y*(t**2-t_min**2)/(2*delta_t)+delta*(t-t_min))/T
@@ -107,12 +102,14 @@ C0 = np.sum(delta_y*(t**2-t_min**2)/(2*delta_t)+delta*(t-t_min))/T
 x_terms = lambda n: a(n)*np.cos(2*n*np.pi*t/T)+b(n)*np.sin(2*n*np.pi*t/T)
 y_terms = lambda n: c(n)*np.cos(2*n*np.pi*t/T)+d(n)*np.sin(2*n*np.pi*t/T)
 
-# Functions returning the total sum of x(t) and y(t) given n harmonics. 
+
+# Functions returning the total sum of x(t) and y(t) given n harmonics.
 def x(n):
     output = A0
     for i in range(1, n + 1):
         output += x_terms(i)
     return output
+
 
 def y(n):
     output = C0
@@ -120,11 +117,12 @@ def y(n):
         output += y_terms(i)
     return output
 
+
 # Choices of n to display
 n = [1, 2, 5, 10, 20, 100]
 
 # plot contour approximations
-fig, axes = plt.subplots(3, 3)
+_, axes = plt.subplots(3, 3)
 
 for i, ax in enumerate(axes.flatten()):
     if i == 0:
