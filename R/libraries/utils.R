42f4133bf01ab13b21aa8125821933be3841e57f
Iver Martinsen <iver.martinsen@outlook.com>
2021-11-22 15:51:10 +0100

    merged and renamed files

--- R/libraries/utils.R
+++ R/libraries/utils.R
@@ -1,3 +1,19 @@
+# Utility functions
+
+
+integrate_num = function(x, a, b, method){
+  # Numerical integration using the trapezoid or the simpsons rule
+  m = length(x) - 1
+  h = (b - a) / m
+  if(method == 'trapezoid'){
+    return(sum((x[1:m-1] + x[2:m])*h/2))    
+  }
+  else if(method == 'simpson'){
+    return((x[1] + x[m+1] + 4*sum(x[seq(2, m, 2)]) + 2*sum(x[seq(3, m-1, 2)]))*h/3)    
+  }
+}
+
+
 make_contrast = function(n, difference = TRUE){
   #' function for making contrast matrix for all possible
   #' differences between n variables
