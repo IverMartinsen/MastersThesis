301b10debad25afe89559a5caea1652f38aba796
Iver Martinsen <iver.martinsen@outlook.com>
2022-03-14 16:47:48 +0100

    fixed something

--- R/scripts/greenland_halibut_uncertainty.R
+++ R/scripts/greenland_halibut_uncertainty.R
@@ -1,17 +1,19 @@
 # Script for analysis of the Greenland halibut test results.
 
+
 library(ggplot2)
 library(reshape2)
 library(viridis)
 source('integral_numerical.R')
 
+
 # Load individual and summary results
-results = read.csv(r'(C:\Users\iverm\OneDrive - UiT Office 365\UiT\Deep learning applied to fish otolith images\Blåkveiteotolitter\Validation Procedure\results.csv)')
-summary = read.csv(r'(C:\Users\iverm\OneDrive - UiT Office 365\UiT\Deep learning applied to fish otolith images\Blåkveiteotolitter\Validation Procedure\summary.csv)')
+results = read.csv(r'(C:\Users\UiT\OneDrive - UiT Office 365\Desktop\Deep learning applied to fish otolith images\Artikkel om blÃ¥kveiteotolitter\Resultater\results.csv)')
+summary = read.csv(r'(C:\Users\UiT\OneDrive - UiT Office 365\Desktop\Deep learning applied to fish otolith images\Artikkel om blÃ¥kveiteotolitter\Resultater\summary.csv)')
 
 
 draw_differences = function(y, title){
-  #' Displays differences between labeled distribution and predicted distribution
+  #' Highlights the area which differs between the labeled distribution and predicted distribution.
   #'
   #' y: predictions to analyze, e.g. 'y1'
   #' title: plot title, e.g. 'Predictions by deep learning'
@@ -51,55 +53,63 @@ draw_differences = function(y, title){
 
 
 # Display difference in age distributions
-draw_differences('y1', 'test')
+draw_differences('y2', 'test')
 
 
-# Compute and print accuracy and loss for all models/sexes
-for(i in 5:7){
-  print(colnames(results)[i])
-  print('')
-  print(paste0('MSE total:   ', mean((results$age - results[, i])**2)))
-  print(paste0('MSE males:   ', mean((results_male$age - results_male[, i])**2)))
-  print(paste0('MSE females: ', mean((results_female$age - results_female[, i])**2)))
-  for(j in 0:2){
-    print('')
-    
-    # Total j-off percentage and mean squared error
-    print(paste0(j, '-off percentage total:   ', sum(abs(results$age - round(results[, i])) <= j)*100 / dim(results)[1])) 
-    
-    # j-off and mean squared error for males
+# Compute and print accuracy and loss for all models/sexes 
+# by iterating over the columns that store the loss.
+for(i in 6:7){
     results_male = results[results$sex == 'male', ]
-    print(paste0(j, '-off percentage males:   ', sum(abs(results_male$age - round(results_male[, i])) <= j)*100 / dim(results_male)[1]))
-    
-    # j-off percentage and mean squared error for females
     results_female = results[results$sex == 'female', ]
-    print(paste0(j, '-off percentage females: ', sum(abs(results_female$age - round(results_female[, i])) <= 2)*100 / dim(results_female)[1]))
-  }
-  print('')
-}
+    
+    print(colnames(results)[i])
+    print('')
+    print(paste0('MSE total:   ', mean((results$age - results[, i])**2)))
+    print(paste0('MSE males:   ', mean((results_male$age - results_male[, i])**2)))
+    print(paste0('MSE females: ', mean((results_female$age - results_female[, i])**2)))
+    
+    print('')
+    print(paste0('CV total:   ', mean(sqrt((2/(1 + results[, i]/results$age) - 1)**2 + (2/(1 + results$age/results[, i]) - 1)**2))))
+    print(paste0('CV males:   ', mean(sqrt((2/(1 + results_male[, i]/results_male$age) - 1)**2 + (2/(1 + results_male$age/results_male[, i]) - 1)**2))))
+    print(paste0('CV females: ', mean(sqrt((2/(1 + results_female[, i]/results_female$age) - 1)**2 + (2/(1 + results_female$age/results_female[, i]) - 1)**2))))
+    
+    for(j in 0:2){
+        print('')
+    
+        # Total j-off percentage and mean squared error
+        print(paste0(j, '-off percentage total:   ', sum(abs(results$age - round(results[, i])) <= j)*100 / dim(results)[1])) 
+    
+        # j-off and mean squared error for males
+        print(paste0(j, '-off percentage males:   ', sum(abs(results_male$age - round(results_male[, i])) <= j)*100 / dim(results_male)[1]))
+    
+        # j-off percentage and mean squared error for females
+        print(paste0(j, '-off percentage females: ', sum(abs(results_female$age - round(results_female[, i])) <= j)*100 / dim(results_female)[1]))
+        }
+    print('')
+    }
 
 
 # Add a small term to the male age to enable pairwise plotting of of both sexes
 df.temp = results
-df.temp$age[which(df.temp$sex == 'male')] = df.temp$age[which(df.temp$sex == 'male')] + 0.3
+df.temp$age[which(df.temp$sex == 'male')] = df.temp$age[which(df.temp$sex == 'male')] + 0.4
 
 # Scatter plot predicted age vs labeled age
-ggplot(df.temp, aes(x = age, y = round(y3))) + 
-  geom_count(aes(colour = factor(sex)), alpha = 1) + 
-  scale_size_area(max_size = 16) + 
-  theme_classic() + 
-  geom_abline(slope = 1, intercept = 0, linetype = 'dotted') + 
-  scale_x_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) +
-  scale_y_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) + 
-  labs(x = 'True age', y = 'Predicted age', title = 'Combined predictions') + 
-  theme(text = element_text(size = 20), plot.title = element_text(hjust = 0.5)) + 
-  scale_colour_discrete(name = 'Sex', labels = c('Female', 'Male'))
-
+ggplot(df.temp, aes(x = age, y = round(y1), color = sex)) + 
+    geom_count(aes(colour = sex), alpha = 1) + 
+    scale_size_area(max_size = 8) + 
+    theme_classic() + 
+    geom_abline(slope = 1, intercept = 0, linetype = 'dotted') + 
+    scale_x_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) +
+    scale_y_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) + 
+    labs(x = 'True age', y = 'Predicted age', title = '') + 
+    theme(text = element_text(size = 10), plot.title = element_text(hjust = 0.5), legend.position = c(0.1, 0.7)) + 
+    scale_color_discrete(name = 'Sex', labels = c('Female', 'Male'))
 
 # Create dataframe for comparing age vs age for length model and deep learning model
 df.temp = melt(results, measure.vars = c('y2', 'y1'))
 # Add a small term to the x-axis for the length predictions to enable pairwise comparison
 df.temp$age[which(df.temp$variable == 'y1')] = df.temp$age[which(df.temp$variable == 'y1')] + 0.3
+
 
 # Plot difference in age-vs-age for length and deep learning
 ggplot(df.temp[which(df.temp$sex == 'female'), ], aes(x = age, y = round(value), color = variable)) + 
@@ -171,9 +181,13 @@ compute_kl_divergence = function(y, sex){
 compute_kl_divergence('y1', 'male')
 
 
+# Helper function to generate colors from the standard ggplot2 palette
+get_colors = function(n) hcl(h = seq(15, 375, length = n + 1), l = 65, c = 100)[1:n]
+
+
 # For each model, for each age, compute MSE contribution
 y_vals = c()
-for(y in c('y1', 'y2', 'y3')){
+for(y in c('y1', 'y2')){
   temp = rep(0, 26)
   for(i in 1:26){
     idx = which(results$age == i)
@@ -182,22 +196,24 @@ for(y in c('y1', 'y2', 'y3')){
   y_vals = c(y_vals, temp)
 }
 
+
 # Create dataframe with age-wise MSE contributions with model as factor variable
 df.temp = data.frame(
-  x = c(1:26, 1:26, 1:26), 
+  x = c(1:26, 1:26), 
   y = y_vals, 
-  f = c(rep('deep_learning', 26), rep('length', 26), rep('combined', 26))
+  f = c(rep('deep_learning', 26), rep('length', 26))
   )
 
+#, fill = factor(f, levels = c('length', 'deep_learning'))
 # Plot MSE contributions against age for all three models
-ggplot(df.temp, aes(x = x, y = y, fill = factor(f, levels = c('length', 'deep_learning', 'combined')))) + 
-  geom_line(position = 'identity', size = 2) + 
-  geom_area(position = 'identity', alpha = 0.6) + 
-  theme_classic() + 
-  scale_x_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) + 
-  theme(text = element_text(size = 20), plot.title = element_text(hjust = 0.5)) + 
-  labs(x = 'Age', y = 'Squared error') + 
-  scale_fill_discrete(name = '', labels = c('Length', 'Image', 'Combined'))
+ggplot(df.temp, aes(x = x, y = y, fill = factor(f, levels = c('length', 'deep_learning')), color = f)) + 
+    geom_area(position = 'identity', alpha = 0.6) + 
+    theme_classic() + 
+    scale_x_continuous(breaks=seq(0, 26, by=2), limits=c(0, 26)) + 
+    theme(text = element_text(size = 10), plot.title = element_text(hjust = 0.5), legend.position = c(0.9, 0.9)) + 
+    labs(x = 'Age', y = 'Squared error') + 
+    scale_fill_discrete(name = '', labels = c('Length', 'Image')) + 
+    scale_color_manual(values = c('black', 'black'), guide = 'none')
 
 
 # Plot standard errors vs predicted age
